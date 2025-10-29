639a14983bf48c158b5b6d1325c7da3c3d34cbf2
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-10-29 22:24:56 +0700

    Initial Commit

---
+++ frontend-finalproject/frontend_finalprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontend_finalprojectApp.swift
+//  frontend-finalproject
+//
+//  Created by Thanyapisit on 29/10/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct frontend_finalprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
