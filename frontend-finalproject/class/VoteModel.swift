06105e1ebba9ecacd53516e0e0f640e5bc5b6c17
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-10-31 11:11:12 +0700

    I think it work 2

---
+++ frontend-finalproject/class/VoteModel.swift
@@ -0,0 +1,21 @@
+//
+//  VoteModel.swift
+//  frontend-finalproject
+//
+
+import Foundation
+
+struct VoteModel: Identifiable, Codable {
+    let id: String
+    let userId: String
+    let pollId: String
+    let selectedOption: String
+    let timestamp: String
+    let previousHash: String?
+    let currentHash: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case userId, pollId, selectedOption, timestamp, previousHash, currentHash
+    }
+}
